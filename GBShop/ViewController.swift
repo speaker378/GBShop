86e6d6c0e63c094eead47625bdb89df58e9ab7f4
speaker378 <speaker378debua@gmail.com>
2022-04-21 21:13:54 +0500

    init

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Сергей Черных on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
