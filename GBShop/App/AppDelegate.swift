3cfd911974fc5d1a4cca6344f512cc680b8973c0
speaker378 <speaker378debua@gmail.com>
2022-04-27 17:03:04 +0500

    covered the code with tests removed the tests from the AppDelegate

--- GBShop/App/AppDelegate.swift
+++ GBShop/App/AppDelegate.swift
@@ -9,52 +9,8 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    let requestFactory = RequestFactory()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        let auth = requestFactory.makeAuthRequestFactory()
-        auth.login(userName: "Somebody", password: "mypassword") { response in
-            switch response.result {
-            case .success(let login):
-                print(login)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-
-        let logoutRequest = requestFactory.makeLogoutRequestFactory()
-        logoutRequest.logout(userId: 123) { response in
-            switch response.result {
-            case .success(let logout):
-                print(logout)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-
-        let registrationRequest = requestFactory.makeRegistrationRequestFactory()
-        let userData = UserDataRequestTest.getUserDataForTest()
-        registrationRequest.registration(user: userData) { response in
-            switch response.result {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-
-        let changeUserDataRequest = requestFactory.makeChangeUserDataFactory()
-        let userData2 = UserDataRequestTest.getUserDataForTest()
-        changeUserDataRequest.change(user: userData2) { response in
-            switch response.result {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-
         return true
     }
 
